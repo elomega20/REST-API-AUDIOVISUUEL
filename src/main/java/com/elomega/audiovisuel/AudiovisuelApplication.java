d2049db77279a346ac7bb70aa12d62c6fb63dd13
el sedy ba <elsedy000@gmail.com>
2023-01-14 13:54:11 +0000

    initial commit

---
+++ src/main/java/com/elomega/audiovisuel/AudiovisuelApplication.java
@@ -0,0 +1,13 @@
+package com.elomega.audiovisuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiovisuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiovisuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elomega/audiovisuel/AudiovisuelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elomega.audiovisuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiovisuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
