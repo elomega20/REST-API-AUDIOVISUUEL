31e745b790d0447385feed8e03bf6e55f00de7ab
el sedy ba <elsedy000@gmail.com>
2023-02-18 11:57:39 +0000

    modifications des enumerations

--- src/main/java/com/elomega/audiovisuel/enumeration/Couleur.java
+++ src/main/java/com/elomega/audiovisuel/enumeration/Couleur.java
@@ -1,15 +1,9 @@
 package com.elomega.audiovisuel.enumeration;
 
 public enum Couleur {
-    VERT("VERT"),
-    JAUNE("JAUNE"),
-    ROUGE("ROUGE"),
-    MARON("MARON"),
-    ROSE("ROSE");
-
-    private String couleur;
-
-    Couleur(String couleur) {
-        this.couleur = couleur;
-    }
+    VERT,
+    JAUNE,
+    ROUGE,
+    MARON,
+    ROSE;
 }
--- src/main/java/com/elomega/audiovisuel/enumeration/Pouvoir.java
+++ src/main/java/com/elomega/audiovisuel/enumeration/Pouvoir.java
@@ -1,13 +1,9 @@
 package com.elomega.audiovisuel.enumeration;
 
 public enum Pouvoir {
-    VOLER("VOLER"),
-    LIRE_DANS_LES_PENSEES("LIRE_DANS_LES_PENSEES"),
-    CHANGER_DASPECT("CHANGER_DASPPECT"),
-    AVOIR_UNE_FORCE_SURHUMAINE("AVOIR_UNE_FORCE_SURHUMAINE"),
-    SE_TELEPORTER("SE_TELEPORTER");
-    private String pouvoir;
-    Pouvoir(String pouvoir) {
-        this.pouvoir = pouvoir;
-    }
+    VOLER,
+    LIRE_DANS_LES_PENSEES,
+    CHANGER_DASPECT,
+    AVOIR_UNE_FORCE_SURHUMAINE,
+    SE_TELEPORTER;
 }
