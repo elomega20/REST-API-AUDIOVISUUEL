bf3afa0eaa5f81478100f2d6776f68c1635079e9
elomega20 <104397369+elomega20@users.noreply.github.com>
2023-03-08 14:10:12 +0000

    Delete ConvertEntityToDtoImplementation.java

--- src/main/java/com/elomega/audiovisuel/service/serviceImpl/ConvertEntityToDtoImplementation.java
+++
@@ -1,6 +0,0 @@
-package com.elomega.audiovisuel.service.serviceImpl;
-
-import com.elomega.audiovisuel.service.ConvertEntityToDto;
-
-public class ConvertEntityToDtoImplementation implements ConvertEntityToDto {
-}
