40867c5d3ac3143b537fd8fed5be6c1e4401fbc5
el sedy <elsedy000@gmail.com>
2023-03-08 13:26:26 +0000

    refactor ActeurRequest to ActeurDto

--- src/main/java/com/elomega/audiovisuel/dto/ActeurDto.java
+++ src/main/java/com/elomega/audiovisuel/dto/ActeurDto.java
@@ -2,7 +2,7 @@ package com.elomega.audiovisuel.dto;
 
 import java.time.LocalDate;
 
-public class ActeurRequest {
+public class ActeurDto {
     private String nom;
     private String prenom;
     private LocalDate dateDeNaissance;
