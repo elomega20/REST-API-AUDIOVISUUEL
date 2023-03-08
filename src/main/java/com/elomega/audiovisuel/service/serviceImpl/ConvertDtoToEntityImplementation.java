63582191babf537ee6a94307e555db6ac411d6e2
el sedy <elsedy000@gmail.com>
2023-03-08 17:20:21 +0000

    modification temporaire

--- src/main/java/com/elomega/audiovisuel/service/serviceImpl/ConvertDtoToEntityImplementation.java
+++ src/main/java/com/elomega/audiovisuel/service/serviceImpl/ConvertDtoToEntityImplementation.java
@@ -10,13 +10,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ConvertDtoToEntityImplementation implements ConvertDtoToEntity {
-    @Autowired
-    private ModelMapper modelMapper;
+   /* @Autowired
+    private ModelMapper modelMapper;*/
     @Override
-    public Acteur convertActeurDtoToActeurEntity(ActeurRequest acteurDto) {
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+    public Acteur convertActeurDtoToActeurEntity(ActeurRequest acteurRequest) {
+        /*modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         Acteur acteur = new Acteur();
-        acteur = modelMapper.map(acteurDto,Acteur.class);
+        acteur = modelMapper.map(acteurDto,Acteur.class);*/
+        Acteur acteur = new Acteur();
+        acteur.setNom(acteurRequest.getNom());
+        acteur.setPrenom(acteurRequest.getPrenom());
+        acteur.setDateDeNaissance(acteurRequest.getDateDeNaissance());
         return acteur;
     }
 
