e8d6f5f10c60282cebcb440e0c5b83b913e80632
el sedy <elsedy000@gmail.com>
2023-03-08 13:12:51 +0000

    add ActeurRequest

---
+++ src/main/java/com/elomega/audiovisuel/dto/ActeurRequest.java
@@ -0,0 +1,9 @@
+package com.elomega.audiovisuel.dto;
+
+import java.time.LocalDate;
+
+public class ActeurRequest {
+    private String nom;
+    private String prenom;
+    private LocalDate dateDeNaissance;
+}
