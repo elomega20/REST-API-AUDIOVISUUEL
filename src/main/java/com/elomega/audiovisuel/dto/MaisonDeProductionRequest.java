85671b507cf790db27e05f80bb9eab7eba92995e
el sedy ba <elsedy000@gmail.com>
2023-03-10 15:32:58 +0000

    add maison de production dto

---
+++ src/main/java/com/elomega/audiovisuel/dto/MaisonDeProductionRequest.java
@@ -0,0 +1,15 @@
+package com.elomega.audiovisuel.dto;
+
+import java.time.LocalDate;
+
+public class MaisonDeProductionRequest {
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private LocalDate anneeDeCreation;
+    private String profileImageUrl;
+    private String role;
+    private String isActive;
+    private String isNotLocked;
+}
---
+++ src/main/java/com/elomega/audiovisuel/dto/MaisonDeProductionResponse.java
@@ -0,0 +1,22 @@
+package com.elomega.audiovisuel.dto;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class MaisonDeProductionResponse {
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private LocalDate anneeDeCreation;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String role;
+    private String[] authorities;
+    private String isActive;
+    private String isNotLocked;
+}
