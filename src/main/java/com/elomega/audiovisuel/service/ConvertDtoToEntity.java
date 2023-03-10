0a0f54f6e469ad565905873dc808c4fc60a4f90f
el sedy ba <elsedy000@gmail.com>
2023-03-10 16:08:02 +0000

    add methode dto maison de production

--- src/main/java/com/elomega/audiovisuel/service/ConvertDtoToEntity.java
+++ src/main/java/com/elomega/audiovisuel/service/ConvertDtoToEntity.java
@@ -2,9 +2,14 @@ package com.elomega.audiovisuel.service;
 
 import com.elomega.audiovisuel.dto.ActeurRequest;
 import com.elomega.audiovisuel.dto.ActeurResponse;
+import com.elomega.audiovisuel.dto.MaisonDeProductionRequest;
+import com.elomega.audiovisuel.dto.MaisonDeProductionResponse;
 import com.elomega.audiovisuel.model.Acteur;
+import com.elomega.audiovisuel.model.MaisonDeProduction;
 
 public interface ConvertDtoToEntity {
     Acteur convertActeurResquestToActeurEntity(ActeurRequest acteurRequest);
     Acteur convertActeurResponseToActeurEntity(ActeurResponse acteurResponse);
+    MaisonDeProduction convertMaisonDeProductionRequestToMaisonDeProductionEntity(MaisonDeProductionRequest maisonDeProductionRequest);
+    MaisonDeProduction convertMaisonDeProductionResponseToMaisonDeProductionEntity(MaisonDeProductionResponse maisonDeProductionResponse);
 }
--- src/main/java/com/elomega/audiovisuel/service/ConvertEntityToDto.java
+++ src/main/java/com/elomega/audiovisuel/service/ConvertEntityToDto.java
@@ -1,8 +1,11 @@
 package com.elomega.audiovisuel.service;
 
 import com.elomega.audiovisuel.dto.ActeurResponse;
+import com.elomega.audiovisuel.dto.MaisonDeProductionResponse;
 import com.elomega.audiovisuel.model.Acteur;
+import com.elomega.audiovisuel.model.MaisonDeProduction;
 
 public interface ConvertEntityToDto {
     ActeurResponse convertActeurEntityToActeurResponse(Acteur acteur);
+    MaisonDeProductionResponse convertMainsonDeProductionEntityToMaisonDeProductionResponse(MaisonDeProduction maisonDeProduction);
 }
