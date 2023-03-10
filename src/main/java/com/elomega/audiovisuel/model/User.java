7606cbe605066111b8cb569d2a737974cce24806
el sedy ba <elsedy000@gmail.com>
2023-03-10 10:15:42 +0000

    add  User

---
+++ src/main/java/com/elomega/audiovisuel/model/User.java
@@ -0,0 +1,45 @@
+package com.elomega.audiovisuel.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+@Data
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String role;
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isNotLocked() {
+        return isNotLocked;
+    }
+
+    public void setNotLocked(boolean notLocked) {
+        isNotLocked = notLocked;
+    }
+}
